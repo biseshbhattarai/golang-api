19c7e20cda0f1f0c51e932d65a238173b0f0ce59
biseshbhattarai <aashishbhattarai49@gmail.com>
2018-09-20 18:37:55 +0545

    Database added

--- main.go
+++ main.go
@@ -4,114 +4,114 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 )
 
 // Book struct (Model)
 type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
-}
-
-// Author struct
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
+	gorm.Model
+	Name   string
+	Pages  string
+	Author string
 }
 
 // Init books var as a slice Book struct
 var books []Book
 
 // Get all books
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
 
 // Get single book
-func getBook(w http.ResponseWriter, r *http.Request) {
+func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // Gets params
-	// Loop through books and find one with the id from the params
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
+
+	db, err := gorm.Open("sqlite3", "book.db")
+	if err != nil {
+		panic("Couldn't connect to the database")
 	}
-	json.NewEncoder(w).Encode(&Book{})
+	defer db.Close()
+	db.Find(&books)
+	json.NewEncoder(w).Encode(books)
 }
 
 // Add new book
 func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(100000000)) // Mock ID - not safe
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
+	db, err := gorm.Open("sqlite3", "book.db")
+	if err != nil {
+		panic("Couldn't connect to the database")
+	}
+	defer db.Close()
+	vars := mux.Vars(r)
+	name := vars["name"]
+	page := vars["page"]
+
+	author := vars["author"]
+	db.Create(&Book{Name: name, Pages: page, Author: author})
+	fmt.Fprintf(w, "New Book successfully added")
 }
 
 // Update book
 func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
+	db, err := gorm.Open("sqlite3", "book.db")
+	if err != nil {
+		panic("Couldn't connect to the database")
 	}
+	defer db.Close()
+	vars := mux.Vars(r)
+	name := vars["name"]
+	author := vars["author"]
+	var book Book
+	db.Where("name = ?", name).Find(&book)
+	book.Author = author
+	db.Save(&book)
+	fmt.Fprintf(w, "Succesfully updated")
 }
 
 // Delete book
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-		json.NewEncoder(w).Encode(books)
+	db, err := gorm.Open("sqlite3", "book.db")
+	if err != nil {
+		panic("Couldn't connect to the database")
 	}
+	defer db.Close()
+	vars := mux.Vars(r)
+	name := vars["name"]
+
+	var book Book
+	db.Where("name = ?", name).Find(&book)
+	db.Delete(&book)
+	fmt.Fprintf(w, "Succesfully deleted")
 }
 
-// Main function
-func main() {
-	// Init router
+func requestRoutes() {
 	r := mux.NewRouter()
 
 	// Hardcoded data - @todo: add database
-	books = append(books, Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}})
-	books = append(books, Book{ID: "2", Isbn: "454555", Title: "Book Two", Author: &Author{Firstname: "Steve", Lastname: "Smith"}})
-	b := Book{ID: "1", Isbn: "438227", Title: "Book One", Author: &Author{Firstname: "John", Lastname: "Doe"}}
-	// Route handles & endpoints
-	fmt.Println(*b.Author)
 	r.HandleFunc("/books", getBooks).Methods("GET")
-	r.HandleFunc("/books/{id}", getBook).Methods("GET")
 	r.HandleFunc("/books", createBook).Methods("POST")
 	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
 
 	// Start server
 	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
 
-// Request sample
-// {
-// 	"isbn":"4545454",
-// 	"title":"Book Three",
-// 	"author":{"firstname":"Harry","lastname":"White"}
-// }
+func initialMigration() {
+	db, err := gorm.Open("sqlite3", "book.db")
+	if err != nil {
+		panic("Couldn't connect to database")
+	}
+	defer db.Close()
+	db.AutoMigrate(&Book{})
+
+}
+
+// Main function
+func main() {
+	initialMigration()
+	requestRoutes()
+
+}
